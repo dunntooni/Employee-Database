58c1c4f890b471cd71381807c9af588e8e0ab6fb
dunntooni <dun15014@byui.edu>
2020-10-29 22:56:41 -0600

    Added more editing features and display options

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def editEmployeeName():
 def editEmployeeExperience():
     print("")
     displayEmployees()
-    employeeID = input("Please input the ID of the employee whose name needs to be changed: ")
+    employeeID = input("Please input the ID of the employee whose experience needs to be changed: ")
     employeeExperience = input("Please input the adjusted experience for the employee: ")
     values = (employeeExperience, employeeID)
     cursor.execute("UPDATE employees SET experience = ? WHERE id = ?", values)
@@ -67,12 +67,48 @@ def editEmployee():
         print("")
         return
 
+def editPositionName():
+    print("")
+    displayPositions()
+    positionID = input("Please input the ID of the position that needs to be changed: ")
+    positionName = input("Please input the new name of the position: ")
+    values = (positionName, positionID)
+    cursor.execute("UPDATE positions SET name = ? WHERE id = ?", values)
+    connection.commit()
+
+
+def editPositionPay():
+    print("")
+    displayPositions()
+    positionID = input("Please input the ID of the position that needs to be changed: ")
+    positionPay = float(input("Please input the adjusted pay for this position: "))
+    values = (positionPay, positionID)
+    cursor.execute("UPDATE positions SET pay = ? WHERE id = ?", values)
+    connection.commit()
+
+
+def editPosition():
+    print("1) Edit Position Title")
+    print("2) Edit Position Pay")
+    print("3) Cancel Edit")
+    editType = input("> ")
+    if editType == "1":
+        editPositionName()
+    elif editType == "2":
+        editPositionPay()
+    else:
+        print("")
+        return
+
 userSelect = "1"
 while userSelect != "7":
     print("What would you like to do?")
     print("1) Add a New Employee")
     print("2) Add a New Position")
     print("3) Edit an Existing Employee")
+    print("4) Edit an Existing Position")
+    print("5) Display Employee List")
+    print("6) Display Position List")
     print("7) Quit")
     print("")
     userSelect = input("> ")
@@ -87,6 +123,15 @@ while userSelect != "7":
         print("")
     elif userSelect == "3":
         editEmployee()
+        print("")
+    elif userSelect == "4":
+        editPosition()
+        print("")
+    elif userSelect == "5":
+        displayEmployees()
+        print("")
+    elif userSelect == "6":
+        displayPositions()
         print("")
 
 connection.close()
